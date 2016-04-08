c01d3fc73f4b513146fa7ec22ecdc4259d7485da
hardrese7 <hard.rese7@gmail.com>
2016-04-08 11:40:55 +0600

    тест

--- Radiator2000/Controls/Tabs/IgolchatiyTab.xaml.cs
+++ Radiator2000/Controls/Tabs/IgolchatiyTab.xaml.cs
@@ -24,5 +24,10 @@ namespace Radiator2000.Controls.Tabs
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
